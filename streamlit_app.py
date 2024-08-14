2a45beff069ccc9344a63efcecb79f2e34556227
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-14 19:33:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,44 @@
-import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import pickle
+from PIL import Image
+import streamlit as st
+import plotly.express as px
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+px.defaults.template = "plotly_dark"
+px.defaults.color_continuous_scale = "reds"
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+with open("data/used_data.pickle", "rb") as f:
+    data = pickle.load(f)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+min_date = data["DATE"].min()
+max_date = data["DATE"].max()
+start_date, end_date = st.sidebar.date_input(label="Rentang Waktu", 
+                                             min_value=min_date,
+                                             max_value=max_date,
+                                             value=[min_date, max_date])
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+categories = ["Semua Kategori"] + list(data["KATEGORI"].value_counts().keys().sort_values())
+category = st.sidebar.selectbox(label="Kategori", options=categories)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+presenters = ["Semua Presenter"] + list(data["PRESENTER"].value_counts().keys().sort_values())
+presenter = st.sidebar.selectbox(label="Presenter", options=presenters)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+outputs = data[(data["DATE"] >= start_date) &
+               (data["DATE"] <= end_date)]
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+if category != "Semua Kategori":
+    outputs = outputs[outputs["KATEGORI"] == category]
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+if presenter != "Semua Presenter":
+    outputs = outputs[outputs["PRESENTER"] == presenter]
 
-    return gdp_df
+#Visualisasi dengan bar chart
+st.header(":video_camera: TOPIK")
+bar_data_1 = outputs.groupby("CG TOPIK")["RATING"].sum().nlargest(10).sort_values(ascending=True)
+fig_1 = px.bar(bar_data_1, x="RATING", color=bar_data_1, orientation='h')
+st.plotly_chart(fig_1)
 
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+#Visualisasi dengan bar chart
+bar_data_2 = outputs.groupby("CG TOPIK")["SHARE"].sum().nlargest(10).sort_values(ascending=True)
+fig_2 = px.bar(bar_data_2, x="SHARE", color=bar_data_2, orientation='h')
+st.plotly_chart(fig_2)
