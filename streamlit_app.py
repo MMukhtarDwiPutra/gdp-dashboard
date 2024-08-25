19ccc0397570b851d07748deb605600712e1a7ff
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-25 20:52:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,14 @@ from PIL import Image
 import streamlit as st
 import plotly.express as px
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 
 px.defaults.template = "plotly_dark"
@@ -91,32 +97,106 @@ with open("data/used_data.pickle", "rb") as f:
 
 ###Predict by time
 # Prepare the feature set and target
-X = df[['START_MINUTES', 'END_MINUTES', 'DURASI']]
-y_presenter = df['PRESENTER']  # Target 1
-y_kategori = df['KATEGORI']    # Target 2
+st.header("Use the best machine learning model : Decision Tree Regressor")
+st.text("Prediksi dengan feature 'START_MINUTES', 'END_MINUTES', 'KATEGORI', 'PRESENTER'")
 
-# Split the data into training and testing sets
-X_train, X_test, y_presenter_train, y_presenter_test = train_test_split(X, y_presenter, test_size=0.3, random_state=42)
-X_train, X_test, y_kategori_train, y_kategori_test = train_test_split(X, y_kategori, test_size=0.3, random_state=42)
+# Initialize LabelEncoder
+label_encoder_kategori = LabelEncoder()
+label_encoder_presenter = LabelEncoder()
 
-# Initialize the models
-model_presenter = RandomForestClassifier(random_state=42)
-model_kategori = RandomForestClassifier(random_state=42)
+# Fit and transform the string data to integers
+df['KATEGORI_NUMBER'] = label_encoder_kategori.fit_transform(df['KATEGORI'])
+df['PRESENTER_NUMBER'] = label_encoder_presenter.fit_transform(df['PRESENTER'])
+
+X = df[['START_MINUTES', 'END_MINUTES', 'KATEGORI_NUMBER', 'PRESENTER_NUMBER']]
+y_share = df['SHARE']  # Target 1
+y_rating = df['RATING']    # Target 2
+
+# Split the data
+X_train_share, X_test_share, y_share_train, y_share_test = train_test_split(X, y_share, test_size=0.3, random_state=42)
+X_train_rating, X_test_rating, y_rating_train, y_rating_test = train_test_split(X, y_rating, test_size=0.3, random_state=42)
+
+scaler = StandardScaler()
+X_train_share_scaled = scaler.fit_transform(X_train_share)
+X_test_share_scaled = scaler.transform(X_test_share)
+X_train_rating_scaled = scaler.fit_transform(X_train_rating)
+X_test_rating_scaled = scaler.transform(X_test_rating)
+
+tree_model_rating = DecisionTreeRegressor(random_state=42)
+tree_model_share = DecisionTreeRegressor(random_state=42)
 
 # Train the models
-model_presenter.fit(X_train, y_presenter_train)
-model_kategori.fit(X_train, y_kategori_train)
+tree_model_rating.fit(X_train_rating_scaled, y_rating_train)
+tree_model_share.fit(X_train_share_scaled, y_share_train)
 
 # Predict on the full test set (to evaluate the model)
-kategori_pred = model_kategori.predict(X_test)
-presenter_pred = model_presenter.predict(X_test)
+rating_pred = tree_model_rating.predict(X_test_rating_scaled)
+share_pred = tree_model_share.predict(X_test_share_scaled)
 
-# Calculate accuracy on the full test set
-accuracy_kategori = accuracy_score(y_kategori_test, kategori_pred)
-accuracy_presenter = accuracy_score(y_presenter_test, presenter_pred)
+# Calculate mse on the full test set
+mse = mean_squared_error(y_share_test, share_pred)
+rmse_share = np.sqrt(mse)
+r2_score_share = tree_model_share.score(X_test_share_scaled, y_share_test)
 
-st.header("Predict with Random Forest Classification")
-st.text(f"Akurasi kategori {accuracy_kategori * 100:.2f}% \t" f"Akurasi presenter {accuracy_presenter * 100:.2f}%")
+mse = mean_squared_error(y_rating_test, rating_pred)
+rmse_rating = np.sqrt(mse)
+r2_score_rating = tree_model_rating.score(X_test_rating_scaled, y_rating_test)
+
+st.text(f"rmse rating Score: {rmse_rating :.2f}")
+st.text(f"r2 rating Score: {r2_score_rating :.2f}")
+st.text(f"rmse share Score: {rmse_share :.2f}")
+st.text(f"r2 share Score: {r2_score_share :.2f}")
+
+st.header("Use the best machine learning model : Random Forest Regressor")
+st.text("Prediksi dengan feature 'DURASI', 'KATEGORI', 'PRESENTER'")
+
+# Initialize LabelEncoder
+label_encoder_kategori = LabelEncoder()
+label_encoder_presenter = LabelEncoder()
+
+# Fit and transform the string data to integers
+df['KATEGORI_NUMBER'] = label_encoder_kategori.fit_transform(df['KATEGORI'])
+df['PRESENTER_NUMBER'] = label_encoder_presenter.fit_transform(df['PRESENTER'])
+
+X = df[['DURASI', 'KATEGORI_NUMBER', 'PRESENTER_NUMBER']]
+y_share = df['SHARE']  # Target 1
+y_rating = df['RATING']    # Target 2
+
+# Split the data
+X_train_share, X_test_share, y_share_train, y_share_test = train_test_split(X, y_share, test_size=0.3, random_state=42)
+X_train_rating, X_test_rating, y_rating_train, y_rating_test = train_test_split(X, y_rating, test_size=0.3, random_state=42)
+
+forest_model_rating = RandomForestRegressor(random_state=42)
+forest_model_share = RandomForestRegressor(random_state=42)
+
+scaler = StandardScaler()
+X_train_share_scaled = scaler.fit_transform(X_train_share)
+X_test_share_scaled = scaler.transform(X_test_share)
+X_train_rating_scaled = scaler.fit_transform(X_train_rating)
+X_test_rating_scaled = scaler.transform(X_test_rating)
+
+# Train the models
+forest_model_rating.fit(X_train_rating_scaled, y_rating_train)
+forest_model_share.fit(X_train_share_scaled, y_share_train)
+
+# Predict on the full test set (to evaluate the model)
+rating_pred = forest_model_rating.predict(X_test_rating_scaled)
+share_pred = forest_model_share.predict(X_test_share_scaled)
+
+# Calculate mse on the full test set
+mse = mean_squared_error(y_share_test, share_pred)
+rmse_share = np.sqrt(mse)
+r2_score_share = forest_model_share.score(X_test_share_scaled, y_share_test)
+
+mse = mean_squared_error(y_rating_test, rating_pred)
+rmse_rating = np.sqrt(mse)
+r2_score_rating = forest_model_rating.score(X_test_rating_scaled, y_rating_test)
+
+st.text(f"rmse rating Score: {rmse_rating :.2f}")
+st.text(f"r2 rating Score: {r2_score_rating :.2f}")
+st.text(f"rmse share Score: {rmse_share :.2f}")
+st.text(f"r2 share Score: {r2_score_share :.2f}")
+
 
 # Input form for time range
 time_range_input = st.text_input("Predict with time range (e.g., 17:58 - 18:06)")
@@ -127,17 +207,98 @@ if time_range_input:
     end_minutes = int(pd.to_datetime(end_time, format='%H:%M').hour * 60 + pd.to_datetime(end_time, format='%H:%M').minute)
     duration = (pd.to_datetime(end_time, format='%H:%M') - pd.to_datetime(start_time, format='%H:%M')).seconds / 60
     
-    # Create a DataFrame with the input data
-    input_data = pd.DataFrame({
-        'START_MINUTES': [start_minutes],
-        'END_MINUTES': [end_minutes],
-        'DURASI': [duration]
-    })
-    
-    # Predict using the trained models
-    presenter_pred = model_presenter.predict(input_data)
-    kategori_pred = model_kategori.predict(input_data)
+    search_df = df[(df["START_MINUTES"] >= start_minutes) & (df["START_MINUTES"] <= end_minutes)]
+    unique_presenter = search_df['PRESENTER'].unique()
+    unique_kategori = search_df["KATEGORI"].unique()
 
-    # Display the results
-    st.write(f"Predicted Presenter: {presenter_pred[0]}")
-    st.write(f"Predicted Kategori: {kategori_pred[0]}")
+    arr_result = []
+
+    scaler = StandardScaler()
+
+    st.header("Prediksi dengan feature 'START_MINUTES', 'END_MINUTES', 'KATEGORI', 'PRESENTER'")
+    for presenter in unique_presenter:
+        for kategori in unique_kategori:
+            kategori_encoded = label_encoder_kategori.transform([kategori])
+            presenter_encoded = label_encoder_presenter.transform([presenter])
+
+            input_data = pd.DataFrame({
+                'START_MINUTES': [start_minutes],
+                'END_MINUTES': [end_minutes],
+                'KATEGORI_NUMBER': kategori_encoded,
+                'PRESENTER_NUMBER': presenter_encoded,
+            })
+            
+            input_data = scaler.fit_transform(input_data)
+            
+            predicted_rating = tree_model_rating.predict(input_data)
+            predicted_share = tree_model_share.predict(input_data)
+            
+            arr_result.append([kategori, presenter, predicted_rating[0], predicted_share[0]])
+
+    data = np.array(arr_result)
+    # Find the row with the maximum value in column 3 (index 2)
+    max_value_rating = data[data[:, 2].astype(float).argmax()]
+    max_value_share = data[data[:, 3].astype(float).argmax()]
+
+    st.text("Prediksi rating tertinggi :")
+    st.text(f"Kategori : {max_value_rating[0]}")
+    st.text(f"Presenter : {max_value_rating[1]}")
+    st.text(f"Rating : {max_value_rating[2]}")
+    st.text(f"Share : {max_value_rating[3]}")
+    st.text("")
+
+    st.text("Prediksi share tertinggi :")
+    st.text(f"Kategori : {max_value_share[0]}")
+    st.text(f"Presenter : {max_value_share[1]}")
+    st.text(f"Rating : {max_value_share[2]}")
+    st.text(f"Share : {max_value_share[3]}")
+
+if time_range_input:
+    start_time, end_time = time_range_input.split(' - ')
+    start_minutes = int(pd.to_datetime(start_time, format='%H:%M').hour * 60 + pd.to_datetime(start_time, format='%H:%M').minute)
+    end_minutes = int(pd.to_datetime(end_time, format='%H:%M').hour * 60 + pd.to_datetime(end_time, format='%H:%M').minute)
+    duration = (pd.to_datetime(end_time, format='%H:%M') - pd.to_datetime(start_time, format='%H:%M')).seconds / 60
+
+    search_df = df[(df["START_MINUTES"] >= start_minutes) & (df["START_MINUTES"] <= end_minutes)]
+    unique_presenter = search_df['PRESENTER'].unique()
+    unique_kategori = search_df["KATEGORI"].unique()
+
+    arr_result = []
+    scaler = StandardScaler()
+
+    st.header("Prediksi dengan feature 'DURASI', 'KATEGORI', 'PRESENTER'")
+    for presenter in unique_presenter:
+        for kategori in unique_kategori:
+            kategori_encoded = label_encoder_kategori.transform([kategori])
+            presenter_encoded = label_encoder_presenter.transform([presenter])
+
+            input_data = pd.DataFrame({
+                'DURASI': [duration],
+                'KATEGORI_NUMBER': kategori_encoded,
+                'PRESENTER_NUMBER': presenter_encoded,
+            })
+            
+            input_data = scaler.fit_transform(input_data)
+            
+            predicted_rating = forest_model_rating.predict(input_data)
+            predicted_share = forest_model_share.predict(input_data)
+            
+            arr_result.append([kategori, presenter, predicted_rating[0], predicted_share[0]])
+
+    data = np.array(arr_result)
+    # Find the row with the maximum value in column 3 (index 2)
+    max_value_rating = data[data[:, 2].astype(float).argmax()]
+    max_value_share = data[data[:, 3].astype(float).argmax()]
+
+    st.text("Prediksi rating tertinggi :")
+    st.text(f"Kategori : {max_value_rating[0]}")
+    st.text(f"Presenter : {max_value_rating[1]}")
+    st.text(f"Rating : {max_value_rating[2]}")
+    st.text(f"Share : {max_value_rating[3]}")
+    st.text("")
+
+    st.text("Prediksi share tertinggi :")
+    st.text(f"Kategori : {max_value_share[0]}")
+    st.text(f"Presenter : {max_value_share[1]}")
+    st.text(f"Rating : {max_value_share[2]}")
+    st.text(f"Share : {max_value_share[3]}")
