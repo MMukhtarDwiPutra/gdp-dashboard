712b665d37498b816e2aa98fcda2151af6deb967
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-27 16:45:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,21 @@ st.set_page_config(
     page_title="Multipage App",
 )
 
+def main():
+    # builds the sidebar menu
+    with st.sidebar:
+        st.page_link('streamlit_app.py', label='Introduction')
+        st.page_link('pages/competition.py', label='Competition Checker')
 
-st.image("data/metrotv.png", use_column_width=True)
+    st.title(f'Introduction')
 
-st.write("Memulai perjalanan penyiarannya dengan visi untuk menghadirkan berita yang cepat dan berkualitas. Kini, Metro TV dikenal sebagai kekuatan utama dalam media berita.")
+    #content
+    st.image("data/metrotv.png", use_column_width=True)
+    
+    st.write("Memulai perjalanan penyiarannya dengan visi untuk menghadirkan berita yang cepat dan berkualitas. Kini, Metro TV dikenal sebagai kekuatan utama dalam media berita.")
+    
+    st.title("*ABOUT US*")
+    st.write("Kami berfokus pada prediksi rating dan share program TV berdasarkan waktu tayang.")
 
-st.title("*ABOUT US*")
-st.write("Kami berfokus pada prediksi rating dan share program TV berdasarkan waktu tayang.")
+if __name__ == '__main__':
+    main()
