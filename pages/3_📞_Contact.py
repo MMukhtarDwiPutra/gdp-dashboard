b31911fbf62755742532a7f2758377d0c244fa0f
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-27 14:44:42 +0700

    Update 3_📞_Contact.py

--- pages/3_📞_Contact.py
+++ pages/3_📞_Contact.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.title("Contact")
+st.set_page_config(
+    page_title="Multipage App",
+    layout="wide",
+)
+
+st.sidebar.success("Select a page above")
+
+
+st.image("data/metrotv.png", use_column_width=True)
+
+st.write("Memulai perjalanan penyiarannya dengan visi untuk menghadirkan berita yang cepat dan berkualitas. Kini, Metro TV dikenal sebagai kekuatan utama dalam media berita.")
+
+st.title("*ABOUT US*")
+st.write("Kami berfokus pada prediksi rating dan share program TV berdasarkan waktu tayang.")
