522c43c25cee67d4e41e03c61b64f78f5319397b
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-27 14:26:42 +0700

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Multipage App",
+)
+
+st.sidebar.success("Select a page above")
+
+
+st.image("data/metrotv.png", use_column_width=True)
+
+st.write("Memulai perjalanan penyiarannya dengan visi untuk menghadirkan berita yang cepat dan berkualitas. Kini, Metro TV dikenal sebagai kekuatan utama dalam media berita.")
+
+st.title("*ABOUT US*")
+st.write("Kami berfokus pada prediksi rating dan share program TV berdasarkan waktu tayang.")
