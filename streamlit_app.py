a74b821a39e7cc4cfc6a095cf559be6fc0323b78
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-28 16:39:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ def main():
     if st.session_state.logged_in:
         if st.button("Logout"):
             st.session_state.logged_in = False
-            # st.experimental_rerun()
+            st.experimental_rerun()
             
         st.markdown(
             """
@@ -37,31 +37,31 @@ def main():
         st.title("*ABOUT US*")
         st.write("Kami berfokus pada prediksi rating dan share program TV berdasarkan waktu tayang.")
 
+# Fungsi untuk mengecek login
+def check_login(username, password):
+    if username in users and users[username] == password:
+        return True
+    return False
+
 if __name__ == '__main__':
     # Data pengguna contoh (dapat disimpan di database atau file terenkripsi)
     users = {"ceyy": "ceyy123"}
-    
-    # Fungsi untuk mengecek login
-    def check_login(username, password):
-        if username in users and users[username] == password:
-            return True
-        return False
     
     # Jika belum login, tampilkan form login
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
     
-        if not st.session_state.logged_in:
-            st.title("Login")
-        
-            username = st.text_input("Username")
-            password = st.text_input("Password", type="password")
-        
-            if st.button("Login"):
-                if check_login(username, password):
-                    st.session_state.logged_in = True
-                    st.success("Login berhasil!")
-                else:
-                    st.error("Username atau password salah")
+    if not st.session_state.logged_in:
+        st.title("Login")
+    
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+    
+        if st.button("Login"):
+            if check_login(username, password):
+                st.session_state.logged_in = True
+                st.success("Login berhasil!")
+            else:
+                st.error("Username atau password salah")
 
     main()
