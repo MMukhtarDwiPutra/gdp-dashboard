b13e3d75cc4900e20682e8125ba4631071f34f28
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-28 16:54:41 +0700

    Update 2_⌨️_Projects.py

--- pages/2_⌨️_Projects.py
+++ pages/2_⌨️_Projects.py
@@ -20,12 +20,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.tools as tls
     
+if st.button("Logout"):
+    st.session_state.logged_in = False
+    
 if not st.session_state.logged_in:
     st.write('<meta http-equiv="refresh" content="0;url=/">', unsafe_allow_html=True)
 else:
-    if st.button("Logout"):
-        st.session_state.logged_in = False
-        
     st.markdown(
         """
         <style>
