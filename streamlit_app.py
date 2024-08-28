c13f069c76496b6cfebb59a4bccba10de6c48c8c
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-28 17:07:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,27 @@ def check_login(username, password):
         return True
     return False
 
-if __name__ == '__main__':
-    # Data pengguna contoh (dapat disimpan di database atau file terenkripsi)
-    users = {"ceyy": "ceyy123"}
-    
-    # Jika belum login, tampilkan form login
-    if 'logged_in' not in st.session_state:
-        st.session_state.logged_in = False
-    
-    if not st.session_state.logged_in:
-        st.title("Login")
-    
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-    
-        if st.button("Login"):
-            if check_login(username, password):
-                st.session_state.logged_in = True
-                st.success("Login berhasil!")
+# if __name__ == '__main__':
+# Data pengguna contoh (dapat disimpan di database atau file terenkripsi)
+users = {"ceyy": "ceyy123"}
 
-                st.write('<meta http-equiv="refresh" content="0;url=/1_📍_Introduction">', unsafe_allow_html=True)
-            else:
-                st.error("Username atau password salah")
-    else:
-        st.write('<meta http-equiv="refresh" content="0;url=/1_📍_Introduction">', unsafe_allow_html=True)
+# Jika belum login, tampilkan form login
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+    st.title("Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if check_login(username, password):
+            st.session_state.logged_in = True
+            st.success("Login berhasil!")
+
+            st.write('<meta http-equiv="refresh" content="0;url=/1_📍_Introduction">', unsafe_allow_html=True)
+        else:
+            st.error("Username atau password salah")
+else:
+    st.write('<meta http-equiv="refresh" content="0;url=/1_📍_Introduction">', unsafe_allow_html=True)
