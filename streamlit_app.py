70edc13ad89c5a7a9ff8e7dce57e60e2f082944f
Muhammad Mukhtar Dwi Putra <35530428+MMukhtarDwiPutra@users.noreply.github.com>
2024-08-28 13:40:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,22 +44,22 @@ def check_login(username, password):
         return True
     return False
 
-# Jika belum login, tampilkan form login
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
-
-if not st.session_state.logged_in:
-    st.title("Login")
-
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        if check_login(username, password):
-            st.session_state.logged_in = True
-            st.success("Login berhasil!")
-        else:
-            st.error("Username atau password salah")
+    # Jika belum login, tampilkan form login
+    if 'logged_in' not in st.session_state:
+        st.session_state.logged_in = False
+    
+    if not st.session_state.logged_in:
+        st.title("Login")
+    
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+    
+        if st.button("Login"):
+            if check_login(username, password):
+                st.session_state.logged_in = True
+                st.success("Login berhasil!")
+            else:
+                st.error("Username atau password salah")
 
     if st.session_state.logged_in:
         main()
